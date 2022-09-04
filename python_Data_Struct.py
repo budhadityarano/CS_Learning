5791bb01747a0727dbc2f4834de47171e28940c9
budhadityarano <43707290+budhadityarano@users.noreply.github.com>
2022-09-04 21:18:30 +0530

    initial version of the file

---
+++ python_Data_Struct.py
@@ -0,0 +1,32 @@
+# Integer
+# Float
+# String
+# Boolean
+
+'''
+list 
+Two ways to create lists
+1. using []   Square brackets
+2. using  list()  class
+
+*** between these two square bracket is faster as in the other way it needs to call list class 
+
+'''
+
+list1 = []
+
+print(list1)
+
+list1 = [1,2,3, 'Aditya', 5.5]
+
+print(list1)
+
+list2 = []
+list2.append('ADITYA')
+
+print(list2)
+
+list3 = [1,2,3,1,4,5,7,7,2,8,9,7]
+print(list3.count(7))
+
+print(dir(list3))
