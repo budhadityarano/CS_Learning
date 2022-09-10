2f5ae79677170679b941d9f161cb44846cf3703d
budhadityarano <43707290+budhadityarano@users.noreply.github.com>
2022-09-10 13:29:46 +0530

    Add files via upload
    Linked List in python

---
+++ lInkedListFullClass.py
@@ -0,0 +1,45 @@
+# linked list with class
+
+
+class linkedListNode:
+    def __init__(self, value, nextNode = None):
+        self.value = value
+        self.nextNode = nextNode
+
+
+class linkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def insert(self, value):
+        node = linkedListNode(value)
+        if self.head is None:
+            self.head = node
+            return
+
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode is None:
+                currentNode.nextNode = node
+                break
+
+            currentNode = currentNode.nextNode
+
+
+    def printLinkedList(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.value , "->", end='')
+            currentNode = currentNode.nextNode
+        print("None")
+        
+
+ll = linkedList()
+ll.printLinkedList()
+ll.insert("3")
+ll.printLinkedList()
+ll.insert("15")
+ll.printLinkedList()
+ll.insert("100")
+ll.printLinkedList()
+
